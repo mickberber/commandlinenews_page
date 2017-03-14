7206a304183a6f6cc5974cddf4545a9c0c51dbc7
Michael Berber <mickberber@icloud.com>
2017-03-14 13:25:42 -0700

    update root to send zip file

--- server/routes/root.js
+++ server/routes/root.js
@@ -2,6 +2,6 @@ const path = require('path');
 
 module.exports = (router) => {
   router.get('/zip', (req, res) => {
-    res.download(path.join(__dirname + './../../public/dist/logo.png'), './logo.png');
+    res.download(path.join(__dirname + './../../public/dist/cln.zip'), 'cln.zip');
   });
 }
