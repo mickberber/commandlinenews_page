6c920bcb2f1a935dca62cc30aad2f9e868740e5a
Michael Berber <mickberber@icloud.com>
2017-03-17 11:57:18 -0700

    add styling

--- public/src/components/ImageBank.js
+++ public/src/components/ImageBank.js
@@ -4,24 +4,37 @@ import styles from './../styles/main.css';
 const ImageBank = () => {
   return (<div>
       <br />
-      <img src='https://www.python.org/static/community_logos/python-powered-w.svg'
-           className={styles.pyimg} />
+      <a href='https://www.python.org'>
+        <img src='https://www.python.org/static/community_logos/python-powered-w.svg'
+             className={styles.pyimg} />
+      </a>
       <br />
       <div className={styles.imgbank}>
+        <hr />
         <div className={styles.supported}>
           Supported Services
         </div>
         <br />
-        <img src='./assets/hacker-news.jpg'
-             className={styles.imghn} />
-        <img src='./assets/cnn.png'
-             className={styles.imgsqr}/>
-        <img src='./assets/The_Guardian.png'
-             className={styles.imgrect} />
-        <img src='./assets/ap.png'
-             className={styles.imgsqr} />
-        <img src='./assets/aljaz.jpg'
-             className={styles.imgsqr} />
+        <a href='https://news.ycombinator.com'>
+          <img src='./assets/hacker-news.jpg'
+               className={styles.imghn} />
+        </a>
+        <a href='http://www.cnn.com'>
+          <img src='./assets/cnn.png'
+               className={styles.imgsqr}/>
+        </a>
+        <a href='https://www.theguardian.com/us'>
+          <img src='./assets/The_Guardian.png'
+               className={styles.imgrect} />
+        </a>
+        <a href='https://bigstory.ap.org'>
+          <img src='./assets/ap.png'
+               className={styles.imgsqr} />
+        </a>
+        <a href='http://www.aljazeera.com/'>
+          <img src='./assets/aljaz.jpg'
+               className={styles.imgsqr} />
+        </a>
       </div>
     </div>);
 }
